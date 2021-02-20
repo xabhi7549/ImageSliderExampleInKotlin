da995d6e678e7c87a1020125c1e4eb611357c115
Abhinav <xabhi7549@gmail.com>
2021-02-20 16:29:06 +0530

    Initial commit

---
+++ app/src/main/java/com/site_valley/imagesliderexampleinkotlin/MainActivity.kt
@@ -0,0 +1,26 @@
+package com.site_valley.imagesliderexampleinkotlin
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.smarteist.autoimageslider.SliderView
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val imageSlider = findViewById<SliderView>(R.id.imageSlider)
+        val imageList: ArrayList<String> = ArrayList()
+        imageList.add("https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__340.jpg")
+        imageList.add("https://images.ctfassets.net/hrltx12pl8hq/4plHDVeTkWuFMihxQnzBSb/aea2f06d675c3d710d095306e377382f/shutterstock_554314555_copy.jpg")
+        imageList.add("https://media.istockphoto.com/photos/child-hands-formig-heart-shape-picture-id951945718?k=6&m=951945718&s=612x612&w=0&h=ih-N7RytxrTfhDyvyTQCA5q5xKoJToKSYgdsJ_mHrv0=")
+        setImageInSlider(imageList, imageSlider)
+    }
+
+    private fun setImageInSlider(images: ArrayList<String>, imageSlider: SliderView) {
+        val adapter = MySliderImageAdapter()
+        adapter.renewItems(images)
+        imageSlider.setSliderAdapter(adapter)
+        imageSlider.isAutoCycle = true
+        imageSlider.startAutoCycle()
+    }
+}
---
+++ app/src/main/java/com/site_valley/imagesliderexampleinkotlin/MySliderImageAdapter.kt
@@ -0,0 +1,41 @@
+package com.site_valley.imagesliderexampleinkotlin
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import com.smarteist.autoimageslider.SliderViewAdapter
+import com.squareup.picasso.Picasso
+import java.util.*
+
+class MySliderImageAdapter() :
+    SliderViewAdapter<MySliderImageAdapter.VH>() {
+    private var mSliderItems = ArrayList<String>()
+    fun renewItems(sliderItems: ArrayList<String>) {
+        mSliderItems = sliderItems
+        notifyDataSetChanged()
+    }
+
+    fun addItem(sliderItem: String) {
+        mSliderItems.add(sliderItem)
+        notifyDataSetChanged()
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup): VH {
+        val inflate: View = LayoutInflater.from(parent.context).inflate(R.layout.image_holder, null)
+        return VH(inflate)
+    }
+
+    override fun onBindViewHolder(viewHolder: VH, position: Int) {
+        Picasso.get().load(mSliderItems[position]).fit().into(viewHolder.imageView)
+    }
+
+    override fun getCount(): Int {
+        return mSliderItems.size
+    }
+
+    inner class VH(itemView: View) : ViewHolder(itemView) {
+        var imageView: ImageView = itemView.findViewById(R.id.imageSlider)
+
+    }
+}
